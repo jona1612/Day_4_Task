6bb44869c65ea383ec91c6e30f549dd146b69405
Jonathan <jonathan16official@gmail.com>
2022-02-09 16:08:07 +0530

    task 3

---
+++ rest.js
@@ -0,0 +1,21 @@
+let request = new XMLHttpRequest();
+
+request.open("GET", "https://restcountries.com/v3.1/all")
+request.send()
+
+request.onload=function(){
+    if (request.status==200){
+        let con= JSON.parse(request.responseText)
+        con.map((c) => {
+            console.log("NAME - "+c.name.official)
+            console.log("REGION - "+c.region)
+            console.log('SUBREGION - '+c.subregion)
+            console.log("POPULATION - "+c.population)
+        });
+        }
+        else{
+            request.onerror=function(){
+                console.log(error)
+            }
+        }
+}
