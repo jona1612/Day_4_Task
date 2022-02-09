602307029cdf548bb36b74b8d28f4cd907fad4df
Jonathan <jonathan16official@gmail.com>
2022-02-09 15:52:34 +0530

    Task

---
+++ obj.js
@@ -0,0 +1,20 @@
+var obj1 = { name: "person 1", age:5 };
+var obj2 = { age:5, name: "person 1" };
+
+var same=true;
+if (Object.keys(obj1).length==Object.keys(obj2).length){
+    for (key in obj1){
+        if (obj1[key] == obj2[key]) {
+              continue;
+        }
+        else
+        {
+            same=false;
+            break;
+        }
+    }
+}
+else{
+    same=false;
+}
+console.log(same);
