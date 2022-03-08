279829d17cb08db17d29f5472cabef2c150ce672
Jonathan <jonathan16official@gmail.com>
2022-03-08 17:23:10 +0530

    task

--- rest.js
+++ rest.js
@@ -7,15 +7,18 @@ request.onload=function(){
     if (request.status==200){
         let con= JSON.parse(request.responseText)
         con.map((c) => {
-            console.log("COUNTRY NAME - "+c.name.official)
-            console.log("REGION - "+c.region)
-            console.log('SUB REGION - '+c.subregion)
-            console.log("POPULATION - "+c.population)
+
+            console.log("COUNTRY NAME - " + c.name.official)
+            console.log("REGION - " + c.region)
+            console.log("SUB REGION - " + c.subregion)
+            console.log("POPULATION - " + c.population)
+            
         });
+    }
+    else
+    {
+        request.onerror = function () {
+            console.log(error)
         }
-        else{
-            request.onerror=function(){
-                console.log(error)
-            }
-        }
+    }
 }
